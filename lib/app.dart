996c1f6514d83fb4a0c25588a470bcf3afeb335f
sathish76 <>
2025-04-02 00:41:24 +0530

    provide stores via provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,30 @@
-
 import 'package:flutter/material.dart';
-import 'package:hola_todo/presentation/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hola_todo/core/di/di.dart';
+import 'package:hola_todo/presentation/mobx/tag/tag_store.dart';
+import 'package:hola_todo/presentation/mobx/task/tasks_store.dart';
+import 'package:hola_todo/presentation/router.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hola Todo', 
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    final baseTheme = Theme.of(context);
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => getIt.get<TasksStore>()),
+        Provider(create: (context) => getIt.get<TagStore>()),
+      ],
+      child: MaterialApp.router(
+        title: 'Hola Todo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          textTheme: GoogleFonts.ubuntuTextTheme(baseTheme.textTheme),
+        ),
+        routerConfig: router,
       ),
-      home: HomeScreen(),
     );
   }
 }
