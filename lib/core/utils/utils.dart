2cdb534cc939ff3f5c697bbdeb25807a3d155bd0
sathish76 <>
2025-04-02 02:54:46 +0530

    add time util

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,5 +1,27 @@
+import 'dart:ui';
+
 import 'package:intl/intl.dart';
+import 'package:relative_time/relative_time.dart';
 
 String formatDate(DateTime date) {
   return DateFormat.yMMMMd().format(date);
+}
+
+String formatDateRelative(DateTime date) {
+  return ucFirst(
+    RelativeTime.locale(
+      const Locale('en'),
+      timeUnits: [
+        TimeUnit.hour,
+        TimeUnit.day,
+        TimeUnit.week,
+        TimeUnit.month,
+        TimeUnit.year,
+      ],
+    ).format(date),
+  );
+}
+
+String ucFirst(String string) {
+  return string.substring(0, 1).toUpperCase() + string.substring(1);
 }
